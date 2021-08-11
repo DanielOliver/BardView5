9baf13df741a6993dad9c99b1ab631ea2f6d28cf
Daniel <danieloliversoftware@gmail.com>
2021-08-11 05:10:10 -0400

    working passport.

--- src/bardview5-server/server.ts
+++ src/bardview5-server/server.ts
@@ -1,13 +1,50 @@
 import express from "express"
 import helmet from "helmet"
-import session from 'cookie-session'
+import expressSession from 'express-session'
 import {buildSchema} from 'graphql'
 import {graphqlHTTP} from 'express-graphql'
 import {rootValue, schema} from "./schema"
 import passport from "passport"
-import OAuth2Strategy from 'passport-oauth2'
+// import OAuth2Strategy from 'passport-oauth2'
 import morgan from 'morgan'
 import jwt from 'jsonwebtoken'
+import {IProfile, OIDCStrategy, VerifyCallback} from 'passport-azure-ad'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+
+// https://github.com/AzureAD/microsoft-authentication-library-for-js/tree/dev/maintenance/passport-azure-ad
+const config = {
+    creds: {
+        responseType: 'code',
+
+        // Required
+        responseMode: 'form_post',
+
+        // Required, the reply URL registered in AAD for your app
+        redirectUrl: 'http://localhost:4000/auth/openid/return',
+
+        // Required if we use http for redirectUrl
+        allowHttpForRedirectUrl: true,
+
+        // Required to set to false if you don't want to validate issuer
+        validateIssuer: false,
+
+        // Recommended to set to true. By default we save state in express session, if this option is set to true, then
+        // we encrypt state and save it in cookie instead. This option together with { session: false } allows your app
+        // to be completely express session free.
+        useCookieInsteadOfSession: true,
+
+        // Required if `useCookieInsteadOfSession` is set to true. You can provide multiple set of key/iv pairs for key
+        // rollover purpose. We always use the first set of key/iv pair to encrypt cookie, but we will try every set of
+        // key/iv pair to decrypt cookie. Key can be any string of length 32, and iv can be any string of length 12.
+        cookieEncryptionKeys: [
+            {'key': '12345678901234567890123456789012', 'iv': '123456789012'},
+            {'key': 'abcdefghijklmnopqrstuvwxyzabcdef', 'iv': 'abcdefghijkl'}
+        ],
+
+        scope: ['profile', 'openid', 'offline_access', 'https://graph.microsoft.com/mail.read'],
+    }
+}
 
 
 const tokenToProfile = async (idToken: string) => {
@@ -21,53 +58,47 @@ const tokenToProfile = async (idToken: string) => {
 };
 
 function serve(isProduction: boolean) {
-    //REDO IT WITH: https://github.com/AzureAD/microsoft-authentication-library-for-js/tree/dev/maintenance/passport-azure-ad#8-samples-and-documentation
-    passport.use(new OAuth2Strategy({
-            authorizationURL: process.env.OAUTH_AUTH_URL || 'test',
-            tokenURL: process.env.OAUTH_TOKEN_URL || 'test',
-            clientID: process.env.OAUTH_CLIENT_ID || 'test',
-            clientSecret: process.env.OAUTH_CLIENT_SECRET || 'test',
-            callbackURL: process.env.OAUTH_CALLBACK_URL || 'test',
-            scope: 'profile email openid',
-            state: true,
-            passReqToCallback: true
-        },
-        // @ts-ignore
-        function (accessToken, refreshToken, profile, cb) {
-            console.log('profile', profile)
-            return cb(null, {
-                'user': 'example',
-                'id': 'test@test.com',
-                'accessToken': accessToken
-            })
-        }
-    ));
-    passport.serializeUser(function (user, done) {
-        console.log('serializeUser', user)
-        // @ts-ignore
-        done(null, user);
-    });
-
-    passport.deserializeUser(function (user, done) {
-        console.log('deserializeUser', user)
-        // @ts-ignore
-        done(null, user)
-    });
-
     const app = express()
     app.use(helmet({contentSecurityPolicy: (isProduction) ? undefined : false}))
     const expiryDate = new Date(Date.now() + 60 * 60 * 1000) // 1 hour
     app.use(morgan('combined'))
-    app.use(session({
-        name: 'session2',
-        keys: ['keytar'],
-        secure: isProduction,
-        httpOnly: isProduction,
-        expires: expiryDate
-    }))
+    app.use(cookieParser());
+    app.use(expressSession({secret: 'keyboard cat', resave: true, saveUninitialized: false}));
+    app.use(bodyParser.urlencoded({extended: true}));
     app.use(passport.initialize());
     app.use(passport.session());
 
+    passport.use(new OIDCStrategy({
+            identityMetadata: process.env.IDENTITY_METADATA || '',
+            clientID: process.env.OAUTH_CLIENT_ID || '',
+            clientSecret: process.env.OAUTH_CLIENT_SECRET || '',
+            responseType: 'code',
+            responseMode: 'form_post',
+            redirectUrl: process.env.OAUTH_CALLBACK_URL || '',
+            allowHttpForRedirectUrl: config.creds.allowHttpForRedirectUrl,
+            validateIssuer: config.creds.validateIssuer,
+            passReqToCallback: false,
+            scope: config.creds.scope,
+            useCookieInsteadOfSession: config.creds.useCookieInsteadOfSession,
+            cookieEncryptionKeys: config.creds.cookieEncryptionKeys,
+            loggingLevel: 'info'
+        },
+        function (iss: string, sub: string, profile: IProfile, access_token: string, refresh_token: string, done: VerifyCallback) {
+            if (!profile.oid) {
+                return done(new Error("No oid found"), null);
+            }
+
+            return done(null, profile);
+        }
+    ));
+
+    passport.serializeUser(function (user, done) {
+        done(null, user);
+    });
+    passport.deserializeUser(function (user, done) {
+        // @ts-ignore
+        done(null, user);
+    });
 
     app.use('/graphql', graphqlHTTP({
         schema: schema,
@@ -75,49 +106,68 @@ function serve(isProduction: boolean) {
         graphiql: true
     }))
 
-    app.get('/logout', function (req, res) {
-        req.logout();
-        console.log('logout', req.user)
-        res.redirect('/graphql');
-    });
-
     app.get(
         "/api",
-        passport.authenticate("oauth2", {session: false}),
+        ensureAuthenticated,
         function (req, res) {
-            var claims = req.authInfo;
-            console.log("User info: ", req.user);
-            console.log("Validated claims: ", claims);
-            // @ts-ignore
-            res.status(200).json({name: claims["name"]});
+            res.status(200).json({user: req.user});
         }
     );
 
+    // @ts-ignore
+    function ensureAuthenticated(req, res, next) {
+        if (req.isAuthenticated()) {
+            return next();
+        }
+        res.redirect('/login');
+    };
 
     app.get(
         "/",
-        passport.authenticate("oauth2", {session: false}),
         function (req, res) {
             var claims = req.authInfo
             console.log("User info: ", req.user)
-            console.log("Validated claims: ", claims)
             // @ts-ignore
-            res.status(200).json({name: claims["name"]})
+            res.status(200).json(req.user)
         }
     );
 
-
-// Redirect the user to the OAuth 2.0 provider for authentication.  When
-// complete, the provider will redirect the user back to the application at
-//     /auth/provider/callback
-    app.get('/auth/provider', passport.authenticate('oauth2'));
-
-    app.get('/auth/provider/callback',
-        passport.authenticate('oauth2', {failureRedirect: '/login'}),
+    app.get('/login',
+        function (req, res, next) {
+            passport.authenticate('azuread-openidconnect',
+                {
+                    // response: res, // supposedly required, but nah
+                    failureRedirect: '/'
+                }
+            )(req, res, next);
+        },
         function (req, res) {
-            // Successful authentication, redirect home.
+            console.log('Login was called in the Sample');
             res.redirect('/');
         });
+
+// POST /auth/openid/return
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   home page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+    app.use('/auth/openid/return',
+        (req, res, next) => {
+            // @ts-ignore
+            passport.authenticate('azuread-openidconnect', {
+                failureRedirect: '/',
+                response: res,
+                successRedirect: '/',
+            })(req, res, next)
+        },
+        function (req, res) {
+            res.redirect('/');
+        });
+
+    app.get('/logout', function (req, res) {
+        req.logout();
+        res.redirect('/');
+    });
 
     app.listen(4000)
 }
