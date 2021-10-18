f917959810dc1310ac1a95e651f48e4dc7caef2a
Daniel <danieloliversoftware@gmail.com>
2021-10-17 21:33:45 -0400

    you like databases? i got some databases

--- server/db/query.sql
+++ server/db/query.sql
@@ -1,2 +1,20 @@
--- name: GetUser :one
-SELECT * FROM public."user" LIMIT 1;
+-- name: UsersFindByUid :many
+SELECT DISTINCT u.*
+FROM role_assignment ra
+         INNER JOIN role r on ra.role_id = r.id
+         INNER JOIN role_permission rp on r.id = rp.role_id
+         INNER JOIN "user" u on evaluate_access_user(rp.conditions, @session_id::bigint, u.id)
+WHERE ra.user_id = @session_id::bigint
+  AND rp.subject = 'user'
+  AND rp.is_active = true
+  AND ra.is_active = true
+  AND r.is_active = true
+  AND u.uid = @uid
+ORDER BY u.id;
+
+-- name: UserInsert :execrows
+INSERT INTO "user" as u (uid, "name", email, tags, created_by)
+VALUES (@uid, @name, @email, @tags, @created_by)
+ON CONFLICT (email) DO NOTHING;
+
+
