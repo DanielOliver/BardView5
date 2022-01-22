da1759b3499d1c5e9b8ad5fd308f9576691df032
Daniel <danieloliversoftware@gmail.com>
2022-01-21 22:19:22 -0500

    Better playwright  tests.

--- e2e/tests/screenshots.spec.ts
+++ e2e/tests/screenshots.spec.ts
@@ -1,15 +1,6 @@
 import { test, expect } from '@playwright/test';
 import * as crypto from 'crypto';
 
-
-// function uuidv4() {
-//   // @ts-ignore
-//   return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-//   // @ts-ignore
-//   (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-//   );
-// }
-
 function createGuid(): string {
   return crypto.randomBytes(16).toString('hex');
 }
@@ -21,17 +12,19 @@ test('landing page to register', async ({ page }) => {
   await page.screenshot({ path: 'screenshots/unauth_landing.png' });
 
   await page.locator('text=Login').first().click();
-  await page.locator('text=ID').isVisible();
-  await page.waitForTimeout(200);
+  await page.waitForLoadState();
+  const idSelector = await page.waitForSelector('text=ID');
+  await idSelector.isVisible();
   await page.screenshot({ path: 'screenshots/unauth_login.png' });
   
   await page.locator('text=Register').click();
-  await page.locator('text=Username').isVisible();
-  await page.waitForTimeout(200);
+  await page.waitForLoadState();
+  const usernameSelector = await page.waitForSelector('text=Username');
+  await usernameSelector.isVisible();
   await page.screenshot({ path: 'screenshots/unauth_register.png' });
 
   const userId = (new Date()).getTime().toString();
-  const password = createGuid(); // (Math.floor(Math.random() * 10000) * 10000 + Math.floor(Math.random() * 5000)).toString();
+  const password = createGuid();
   const email = userId + "@test.com";
 
   const emailField = await page.locator('input[name="traits.email"]');
@@ -45,8 +38,13 @@ test('landing page to register', async ({ page }) => {
   await usernameField.type(userId);
   await page.locator('.btn-primary').click();
    
-  await page.locator('text=Worlds').isVisible();
-  await page.waitForTimeout(2200);
+  await page.waitForLoadState();
+  const worldSelector = await page.waitForSelector('text=Worlds');
+  await worldSelector.isVisible();
+
+  const layoutSelector = await page.waitForSelector(`text=${userId}`);
+  await layoutSelector.isVisible();
+
   await page.screenshot({ path: 'screenshots/registered.png' });
 });
 
