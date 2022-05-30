14c538c99e8c48c6c8e743167c28faaaea58570b
Daniel <danieloliversoftware@gmail.com>
2022-05-30 12:47:43 -0400

    graphql minimal working example

--- server/bv5/api.graphql.go
+++ server/bv5/api.graphql.go
@@ -1,6 +1,7 @@
 package bv5
 
 import (
+	"context"
 	_ "embed"
 	"github.com/graph-gophers/graphql-go"
 	"github.com/graph-gophers/graphql-go/relay"
@@ -12,10 +13,23 @@ var graphqlSchema string
 
 type graphqlQuery struct{}
 
-func (_ *graphqlQuery) Hello() string { return "Hello, world!" }
+func (_ *graphqlQuery) Setting(ctx context.Context, args struct{ ID graphql.ID }) (*settingResolver, error) {
+	return &settingResolver{}, nil
+}
 
 func GraphqlHandler() http.Handler {
 	opts := []graphql.SchemaOpt{graphql.MaxDepth(4)}
 	schema := graphql.MustParseSchema(graphqlSchema, &graphqlQuery{}, opts...)
 	return &relay.Handler{Schema: schema}
+}
+
+type settingResolver struct {
+}
+
+func (s *settingResolver) ID() graphql.ID {
+	return "1"
+}
+
+func (s *settingResolver) Name() string {
+	return "example"
 }
